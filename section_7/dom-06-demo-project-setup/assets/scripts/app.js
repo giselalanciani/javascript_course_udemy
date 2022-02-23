d6c7c245e3bf3b80b7d39df6a6b9f8979472cc6f
Gisela Lanciani <giselalanciani@gmail.com>
2022-02-23 08:54:16 -0300

    section 07 - dom-06-demo-project-setup

---
+++ section_7/dom-06-demo-project-setup/assets/scripts/app.js
@@ -0,0 +1,5 @@
+const addMovieModal = document.addElementById("add-modal");
+//const addMovieModal = document.querySelector("#add-modal");
+//const addMovieModal = document.body.children[1];
+
+console.log(addMovieModal);
